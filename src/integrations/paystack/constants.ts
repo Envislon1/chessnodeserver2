6d34b224387f76678f5ea0f23fb097fff52dbd7b
Demyke93 <gracergysolary@gmail.com>
2025-04-30 21:05:03 +0100

    delete

--- src/integrations/paystack/constants.ts
+++
@@ -1,3 +0,0 @@
-
-// Publishable API key for Paystack
-export const PAYSTACK_PUBLIC_KEY = "pk_test_1972a069020457bfe6d8e9bab7a74aedb52d6073";
--- src/services/matchServiceImport.ts
+++
@@ -1,6 +0,0 @@
-
-// This file is a workaround for import issues
-import { matchService } from './matchService';
-import { socketChessService } from './socketChessService';
-
-export { matchService, socketChessService };
